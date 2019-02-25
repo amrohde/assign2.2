ce2e06bd68a2a2e84c84582b1ec0c07ec6e61eb0
amrohde <amrohde@unomaha.edu>
2019-02-24 22:04:39 -0600

    fixed errors

--- mfscrm/settings.py
+++ mfscrm/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'r1ftdlojrhfx+t5#3@0ff7f&qbz1k3)kt%fb88p4zoce5@___k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
 
 
 # Application definition
@@ -126,4 +126,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
